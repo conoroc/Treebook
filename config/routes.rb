d2db2cc2dc6857317a23769e3d8877bc8645f041
Conor O'Callaghan <conor_oc@ymail.com>
2013-01-25 23:07:17 +0000

    custom routes tests

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Treebook::Application.routes.draw do
   end
 
   resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
+  root :to => 'statuses#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -58,7 +60,7 @@ Treebook::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-root :to => 'statuses#index'
+
 
   # See how all your routes lay out with "rake routes"
 
