fcfe120fd5ab5cbc2dee95c8663f320f1311b02a
Conor O'Callaghan <conor_oc@ymail.com>
2013-01-26 21:36:13 +0000

    profile bug

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -29,12 +29,23 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "user should have a profile name without spaces" do
-    user = User.new
+    user = User.new(first_name: 'conor', last_name: "O'Callaghan", email: 'conor2@mail.com' )
+    user.password = user.password_confirmation = 'asdfasdf'
+
     user.profile_name = "profile name with spaces"
 
     assert !user.save
     assert !user.errors[:profile_name].empty?
     assert user.errors[:profile_name].include?("Must be formatted correctly")
 
+  end
+
+  test "user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'conor', last_name: "O'Callaghan", email: 'conor2@mail.com' )
+    user.password = user.password_confirmation = 'asdfasdf'
+
+    user.profile_name = 'conoroc_2'
+
+    assert user.valid?
   end
 end
