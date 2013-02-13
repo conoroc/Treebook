bc1ef7bfa6517c4058981dab472a8b442f280a2f
Conor O'Callaghan <conor_oc@ymail.com>
2013-02-13 02:09:45 +0000

    shoulda and friendship and test

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should have_many(:user_friendships)
+  should have_many(:friends)
+
   test "user should enter a first name" do
     user = User.new
     assert !user.save
@@ -29,7 +32,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "user should have a profile name without spaces" do
-    user = User.new(first_name: 'conor', last_name: "O'Callaghan", email: 'conor2@mail.com' )
+    user = User.new(first_name: 'conor', last_name: "O'Callaghan", email: 'conor2@mail.com')
     user.password = user.password_confirmation = 'asdfasdf'
 
     user.profile_name = "profile name with spaces"
@@ -41,11 +44,24 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "user can have a correctly formatted profile name" do
-    user = User.new(first_name: 'conor', last_name: "O'Callaghan", email: 'conor2@mail.com' )
+    user = User.new(first_name: 'conor', last_name: "O'Callaghan", email: 'conor2@mail.com')
     user.password = user.password_confirmation = 'asdfasdf'
 
     user.profile_name = 'conoroc_2'
 
     assert user.valid?
   end
+  test "no error when trying to access a friend list" do
+    assert_nothing_raised do
+      users(:conor).friends
+    end
+  end
+
+  test "that creating friendships on a user works" do
+    users(:conor).friends << users(:mike)
+    users(:conor).friends.reload
+    assert users(:conor).friends.include?(users(:mike))
+  end
+
+
 end
