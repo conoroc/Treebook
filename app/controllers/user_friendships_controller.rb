e8ad43ebfc975bb6abbecae559ade3407d8edaf0
Conor O'Callaghan <conor_oc@ymail.com>
2013-02-14 23:08:44 +0000

    more pain the ass - added friendships

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -1,7 +1,7 @@
 class UserFriendshipsController < ApplicationController
   before_filter :authenticate_user!, only: [:new]
-	def new
-		if params[:friend_id]
+  def new
+    if params[:friend_id]
       @friend = User.where(profile_name: params[:friend_id]).first
       raise ActiveRecord::RecordNotFound if @friend.nil?
       @user_friendship = current_user.user_friendships.new(friend: @friend)
@@ -11,5 +11,18 @@ class UserFriendshipsController < ApplicationController
     end
   rescue ActiveRecord::RecordNotFound
     render file: 'public/404', status: :not_found
-	end
+  end
+
+  def create
+    if params[:user_friendship] && params[:user_friendship].has_key?(:friend_id)
+      @friend = User.where(profile_name: params[:user_friendship][:friend_id]).first
+      @user_friendship = current_user.user_friendships.new(friend: @friend)
+      @user_friendship.save
+      flash[:success] = "you are now friends with #{@friend.full_name}"
+      redirect_to profile_path(@friend)
+    else
+      flash[:error] = "Friend Required"
+      redirect_to root_path
+    end
+  end
 end
